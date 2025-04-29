21cee3c31582caa6dc961401060b12e57cb3c77e
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2025-04-29 07:09:17 +0530

    Learning basics

---
+++ Rock-Paper-Scissors/main.py
@@ -0,0 +1,13 @@
+def getChoices():
+    playerChoice='rock'
+    computerChoice='paper'
+
+    return playerChoice
+getChoices()
+
+def greating():
+    print('hello coder')
+    return 'hi'
+
+responce = greating()
+print(responce)
