7bcbb228d4ad99ada192a37a798e084ef8e41e49
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2025-05-02 07:15:11 +0530

    Arithmatic operators

--- Test/main.py
+++ Test/main.py
@@ -20,13 +20,32 @@
 # print (name)
 # # this is a commented line
 # print (my_name) #this is an inline comment
-name = 'bob'
-age = 30
-print (type(name)== str)
-print(isinstance(age, float))
-print(isinstance(age, int))
 
-length=int('20') 
-height=10
-print(isinstance(length, int))
-print(isinstance(height, int))
+
+# name = 'bob'
+# age = 30
+# print (type(name)== str)
+# print(isinstance(age, float))
+# print(isinstance(age, int))
+
+# length=int('20') 
+# height=10
+# print(isinstance(length, int))
+# print(isinstance(height, int))
+
+age=20
+
+# arithmatic operators
+add =1 +1
+sub= 2-1
+div=4/2
+mul=2*10
+remainder=4%3
+power=4**2
+floorDivition=5//2 #it gives the nearest whole value  (2.5 but it will give 2)
+
+age=10
+age += 5
+print(age)
+
+
