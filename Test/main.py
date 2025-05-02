c679b9a09c52bad57ba598208bb4e166e467155c
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2025-05-02 07:07:20 +0530

    data types

--- Test/main.py
+++ Test/main.py
@@ -10,15 +10,23 @@
 # print('hello worl')
 # print('hello world')
 
+
+# __Name__= 'fanboy'; print(__Name__)
+# HEIGH= 'name is HEIGH'
+# my_name='Hello world'
+# # print (__Name__)
+# print (age)
+# print (HEIGH)
+# print (name)
+# # this is a commented line
+# print (my_name) #this is an inline comment
 name = 'bob'
 age = 30
-__Name__= 'fanboy'; print(__Name__)
-HEIGH= 'name is HEIGH'
-my_name='Hello world'
-# print (__Name__)
-print (age)
-print (HEIGH)
-print (name)
-# this is a commented line
-print (my_name) #this is an inline comment
+print (type(name)== str)
+print(isinstance(age, float))
+print(isinstance(age, int))
 
+length=int('20') 
+height=10
+print(isinstance(length, int))
+print(isinstance(height, int))
