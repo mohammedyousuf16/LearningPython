6ca66d1304468b99749d3cca4735cf7337d6d5a2
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2025-05-02 06:53:26 +0530

    variables

--- Test/main.py
+++ Test/main.py
@@ -1,12 +1,22 @@
-print('h')
-print('he')
-print('hel')
-print('hell')
-print('hello')
-print('hello ')
-print('hello w')
-print('hello wo')
-print('hello wor')
-print('hello worl')
-print('hello world')
+# print('h')
+# print('he')
+# print('hel')
+# print('hell')
+# print('hello')
+# print('hello ')
+# print('hello w')
+# print('hello wo')
+# print('hello wor')
+# print('hello worl')
+# print('hello world')
 
+name = 'bob'
+age = 30
+__Name__= 'fanboy'
+HEIGH= 'name is HEIGH'
+my_name='Hello world'
+print (__Name__)
+print (age)
+print (HEIGH)
+print (name)
+print (my_name)
