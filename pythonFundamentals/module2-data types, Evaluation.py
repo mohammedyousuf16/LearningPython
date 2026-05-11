b35346080dd255b4a7016754d22c2822ba37038c
unknown <mohammedyousuf16.md@gmail.com>
2026-05-11 10:36:26 +0530

    more about print and strings

--- pythonFundamentals/module2-data types, Evaluation.py
+++ pythonFundamentals/module2-data types, Evaluation.py
@@ -6,3 +6,17 @@ print(-5) #output: -5
 
 2 ** 2 ** 3 #exponentiation is evaluated from right to left, so this is equivalent to 2 ** (2 ** 3) which is 2 ** 8 = 256
 print(2 ** 2 ** 3) #output: 256
+
+# more about print and strings
+
+len("Hello") #output: 5
+
+print('Hello world') 
+print ('learn python')
+
+print('Hello world', end='.') 
+print ('learn python')
+
+first_name = 'John'
+print('your first name is', first_name, 'Welcome to Python!') #output: your first name is John Welcome to Python!
+print('your first name is', first_name, 'Welcome to Python!', sep='-') #output: your first name is-John-Welcome to Python!
