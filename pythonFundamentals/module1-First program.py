450d9b835b60660d6b632fd3ffbb74b0b77744f1
unknown <mohammedyousuf16.md@gmail.com>
2026-05-11 07:45:56 +0530

    inserting new line using \n

--- pythonFundamentals/module1-First program.py
+++ pythonFundamentals/module1-First program.py
@@ -6,3 +6,6 @@ print('and having fun')
 
 # 2 sentence in a single print function
 print('I\'m lerarning python', 'and having fun')
+
+# using \n to put sting into new line
+print('I\'m lerarning\npython')
