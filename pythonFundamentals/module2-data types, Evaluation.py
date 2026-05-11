d9fab8e8f39f1e89590f8c9fca93391d4e5f62d8
unknown <mohammedyousuf16.md@gmail.com>
2026-05-11 10:30:49 +0530

    more about the operators

---
+++ pythonFundamentals/module2-data types, Evaluation.py
@@ -0,0 +1,8 @@
+# More about operators
+print(2 + 3)  
+print(5 - 2)
+print(+12) #output: 12
+print(-5) #output: -5
+
+2 ** 2 ** 3 #exponentiation is evaluated from right to left, so this is equivalent to 2 ** (2 ** 3) which is 2 ** 8 = 256
+print(2 ** 2 ** 3) #output: 256
