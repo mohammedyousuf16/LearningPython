574451474d1f772c5112ea2403313232a9760eaa
unknown <mohammedyousuf16.md@gmail.com>
2026-05-11 07:43:12 +0530

    two sentence in single print function

--- pythonFundamentals/module1-First program.py
+++ pythonFundamentals/module1-First program.py
@@ -2,3 +2,7 @@ print('hello world')
 
 # by using \ you can skip the char 
 print('I\'m lerarning python')
+print('and having fun')
+
+# 2 sentence in a single print function
+print('I\'m lerarning python', 'and having fun')
