5e8b8655dafe2f157fe0914a86e82c139b98f835
unknown <mohammedyousuf16.md@gmail.com>
2026-05-11 07:53:01 +0530

    Variables in python

--- pythonFundamentals/module1-First program.py
+++ pythonFundamentals/module1-First program.py
@@ -9,3 +9,10 @@ print('I\'m lerarning python', 'and having fun')
 
 # using \n to put sting into new line
 print('I\'m lerarning\npython')
+
+# Variables
+greeting='Hello, friend!'
+print(greeting)
+#variables value change if you re-assign to a differnt value
+greeting='Hi, everybody!'
+print(greeting)
