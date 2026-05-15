8aff530a00230aeca4b07b396bc496559f2f9e8d
unknown <mohammedyousuf16.md@gmail.com>
2026-05-15 06:01:38 +0530

    functions with parameters

--- pythonFundamentals/module3-functions & Exceptions.py
+++ pythonFundamentals/module3-functions & Exceptions.py
@@ -3,3 +3,22 @@
 def greet():
     print('Hello World from a function!')
 greet()
+
+inputNumber = [5.0, 19.8, 3.14, 2.71, 23.9]
+def getAverage(Number):
+    sum = 0
+    for num in Number:
+        sum += num
+    average = sum / len(Number)
+    print(average)
+
+getAverage(inputNumber)
+
+def printLetterCount(text, letter):
+    count = 0
+    for char in text:
+        if char == letter:
+            count += 1
+    print(count)
+
+printLetterCount('hello world, welcome to my world of python learning that i have started', 'e')
