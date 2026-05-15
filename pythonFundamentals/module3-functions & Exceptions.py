2b1fff20cd359bb62ad4e668580ebab2c78a2da2
unknown <mohammedyousuf16.md@gmail.com>
2026-05-15 06:07:33 +0530

    default parameters values

--- pythonFundamentals/module3-functions & Exceptions.py
+++ pythonFundamentals/module3-functions & Exceptions.py
@@ -1,8 +1,10 @@
 # introduction to functions
-
+'''
 def greet():
     print('Hello World from a function!')
 greet()
+
+# functions with parameters
 
 inputNumber = [5.0, 19.8, 3.14, 2.71, 23.9]
 def getAverage(Number):
@@ -22,3 +24,17 @@ def printLetterCount(text, letter):
     print(count)
 
 printLetterCount('hello world, welcome to my world of python learning that i have started', 'e')
+'''
+# default parameters values
+
+print('hello', 'how are you?', end='\n', sep='-')
+
+def printLetterCount(text='this is a default string', letter='a'):
+    count = 0
+    for char in text:
+        if char == letter:
+            count += 1
+    print(count)
+
+printLetterCount('how many letters a are in this sentence?')
+printLetterCount()
