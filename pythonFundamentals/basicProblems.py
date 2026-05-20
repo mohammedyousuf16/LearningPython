522077ce1015521534ae63a14209a851869aec78
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-05-20 12:54:04 +0530

    question 2

--- pythonFundamentals/basicProblems.py
+++ pythonFundamentals/basicProblems.py
@@ -1,5 +1,6 @@
 # Question 1
 #  Write a program that asks for a number and prints wheather it is positive, negative or zero
+'''
 num = int(input('Enter a number:'))
 if num > 0:
     print('the number', num ," is Positive")
@@ -7,3 +8,13 @@ elif num<0:
     print('the number', num ," is Negative")
 else:
     print('the number is zero')
+'''
+# Question 2
+# Create a program if a person is eligible to vote 
+
+age = int(input('Enter your age: '))
+
+if age>18:
+    print('you are eligible to vote')
+else:
+    print('you are not eligible to vote. Sorry!')
