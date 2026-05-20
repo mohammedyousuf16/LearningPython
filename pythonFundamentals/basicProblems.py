bf7fd46f2055bb39c471da1b438f42bf9a2221c5
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-05-20 13:02:41 +0530

    question 3

--- pythonFundamentals/basicProblems.py
+++ pythonFundamentals/basicProblems.py
@@ -8,7 +8,6 @@ elif num<0:
     print('the number', num ," is Negative")
 else:
     print('the number is zero')
-'''
 # Question 2
 # Create a program if a person is eligible to vote 
 
@@ -18,3 +17,13 @@ if age>18:
     print('you are eligible to vote')
 else:
     print('you are not eligible to vote. Sorry!')
+'''
+
+# Question 3
+# Write a program that take a number from the user and prints Even if its even otherwise Odd
+
+num = int(input('Enter a number to check Even or Odd: '))
+if num % 2 == 0: 
+    print('It is Even number')
+else:
+    print('It is Odd number')
