3bd7026c8a59796b79805f2d5456665558c6e877
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-05-20 13:09:17 +0530

    question 4

--- pythonFundamentals/basicProblems.py
+++ pythonFundamentals/basicProblems.py
@@ -1,3 +1,4 @@
+# Solve using If statements
 # Question 1
 #  Write a program that asks for a number and prints wheather it is positive, negative or zero
 '''
@@ -17,7 +18,6 @@ if age>18:
     print('you are eligible to vote')
 else:
     print('you are not eligible to vote. Sorry!')
-'''
 
 # Question 3
 # Write a program that take a number from the user and prints Even if its even otherwise Odd
@@ -27,3 +27,27 @@ if num % 2 == 0:
     print('It is Even number')
 else:
     print('It is Odd number')
+'''
+# Solve using Match Statements
+# Question 4
+# Ask user to enter a day number (1-7) and print the corresponding day 
+
+num = int(input('enter a number between 1 to 7:'))
+
+match num:
+    case 1:
+        print('day 1: Mon')
+    case 2:
+        print('day 2: Tue')
+    case 3:
+        print('day 3: wed')
+    case 4:
+        print('day 4: Thu')
+    case 5:
+        print('day 5: Fri')
+    case 6:
+        print('day 6: Sat')
+    case 7:
+        print('day 7: Sun')
+    case _:
+        print('Enter a vaild number')
