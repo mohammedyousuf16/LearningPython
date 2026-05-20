8ea6f77592804c3e78cbf65bf0965c4c9812a25b
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-05-20 12:50:30 +0530

    question 1

---
+++ pythonFundamentals/basicProblems.py
@@ -0,0 +1,9 @@
+# Question 1
+#  Write a program that asks for a number and prints wheather it is positive, negative or zero
+num = int(input('Enter a number:'))
+if num > 0:
+    print('the number', num ," is Positive")
+elif num<0:
+    print('the number', num ," is Negative")
+else:
+    print('the number is zero')
