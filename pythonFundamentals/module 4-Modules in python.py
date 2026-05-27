223ac9330c08c107ef180516a354ca56abc06f32
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-05-27 13:14:25 +0530

    set operations

--- pythonFundamentals/module 4-Modules in python.py
+++ pythonFundamentals/module 4-Modules in python.py
@@ -13,7 +13,6 @@ r = requests.get("https:www.google.com")
 print(math.sqrt(16))
 mymodule.hello()
 
-'''
 
 # Sets and sets methods in python
 # sets are unordered, unique collections
@@ -29,5 +28,17 @@ s.discard(23445) # discard means only remove if the number is in the set
 s.pop() # removes random elements form the set
 print(s)
 
+'''
 
+# Set operations
 
+a = {23,4,6,1}
+b = {26, 23, 45, 5, 1}
+c= a.union(b) # c has the elements from both the sets no duplicats are allowed
+print(c) 
+d= a.intersection(b) # d has the elements that are same in both the sets
+print(d) 
+e= a.difference(b) # elements in a which are not in b
+f= b.difference(a) # elements in b which are not in a
+print(e)
+print(f)
