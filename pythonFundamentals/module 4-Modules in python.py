912a7cb2bd8e0bc6f9d6479a398ca20fc3d618e5
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-05-27 13:03:14 +0530

    sets in python

--- pythonFundamentals/module 4-Modules in python.py
+++ pythonFundamentals/module 4-Modules in python.py
@@ -2,7 +2,7 @@
 There are two types of modules in python
 - Built in modules
 - External modules
-'''
+
 import math
 import os # if you import and dont use it it will be in gray until you use it
 import mymodule #you can import your own module from other py file
@@ -12,3 +12,22 @@ import requests
 r = requests.get("https:www.google.com")
 print(math.sqrt(16))
 mymodule.hello()
+
+'''
+
+# Sets and sets methods in python
+# sets are unordered, unique collections
+s = {1,3,6,9,4}
+
+print(s, type(s))
+#print(s[3]) # sets are unortdered so this will show an error
+s.add(32)
+s.add(324) # cant add the number which is already in the set
+print(s)
+s.remove(1)
+s.discard(23445) # discard means only remove if the number is in the set
+s.pop() # removes random elements form the set
+print(s)
+
+
+
