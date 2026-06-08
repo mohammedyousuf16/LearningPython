d2ad39eb184de1d0e95c8db1bc1a680321f065d9
unknown <mohammedyousuf16.md@gmail.com>
2026-06-08 12:19:12 +0530

    OOPS class and objects

---
+++ pythonFundamentals/OOPS.py
@@ -0,0 +1,10 @@
+class Employee:
+   company = "HP"
+
+   def get_salary(self):
+    return 34000
+e = Employee() # An Object of class Employee is created here print(e.get_salary()) # Employee e's get salary method is calle
+print(e.get_salary()) # output: 34000
+
+e2 = Employee()
+print(e2.get_salary())
