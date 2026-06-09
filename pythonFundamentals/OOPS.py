d7664158f80c8222146d5a9e6d8e491206953391
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-09 08:10:03 +0530

    Instance and class attribute

--- pythonFundamentals/OOPS.py
+++ pythonFundamentals/OOPS.py
@@ -9,7 +9,7 @@ print(e.get_salary()) # output: 34000
 
 e2 = Employee()
 print(e2.get_salary())
-'''
+
 #Constructor in Python
 class Employee:
    def __init__(self,slary, name, bond):
@@ -27,3 +27,28 @@ class Employee:
 e1 = Employee(32000, 'John', 5)
 print(e1.get_salary())
 e1.get_Info()
+
+'''
+
+# instance and class attribute
+
+class Employee:
+   company = 'Asus'
+   def __init__(self,slary, name, bond, company):
+     self.salary = slary
+     self.name = name   
+     self.bond = bond
+     self.company= company
+
+   def get_salary(self):
+    return self.salary
+   
+   def get_Info(self):
+     print(f'Employee name is {self.name} and his salary is {self.salary} and his bond is {self.bond} years')  
+
+e1= Employee(23000, 'john', 3, 'microsoft')
+print(e1.company) # will always print instance attribute
+print(Employee.company)
+
+#Object introspection
+print(dir(e1))
