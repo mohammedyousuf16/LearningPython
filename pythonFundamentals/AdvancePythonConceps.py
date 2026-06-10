d711e613ec9b9b08cca57c92f9a768f59639cb35
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-10 20:06:22 +0530

    Exception handeling and custom Errors

--- pythonFundamentals/AdvancePythonConceps.py
+++ pythonFundamentals/AdvancePythonConceps.py
@@ -1,5 +1,5 @@
 # Dunder/Magic methods
-
+'''
 class Employee:
     company = "HP"
     def __init__(self, name, salary):
@@ -21,3 +21,62 @@ print(len(e))
 # print(e.name, e.salary)
 # print(str(e))
 # print(repr(e))
+
+'''
+#Exception handeling and custom Errors
+
+
+while False:
+    try: 
+        a = int(input("Enter number 1: "))
+        b = int(input("Enter number 2: "))
+        print(f"The division is {a / b}")
+
+    except ValueError:
+        print("Please dont perform bad typecasts")
+    
+    except ZeroDivisionError:
+        print("Hey dont divide by 0")
+
+    except Exception as e:
+        print("Unknown error occurred!", e)
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+if b == 0:
+    raise ValueError("Please dont divide by 0")
+
+print(f"The division is {a / b}")
+
+
+# Else
+try:
+    a = 345/10
+
+except Exception as e:
+    print(e)
+
+# Gets executed when there is no error in the try block 
+else:
+    print("Hey I am good")
+
+# Finally
+
+def divide(a, b):
+    try:
+        c = a/b 
+        print(c)
+        return c
+
+    except Exception as e:
+        print(e)
+        return None
+
+    # This is always executed no matter if try completely executes or not 
+    finally: 
+        print("This is always executed")
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+divide(a, b)
