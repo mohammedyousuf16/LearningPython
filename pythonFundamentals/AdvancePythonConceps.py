1ced7451b81d6076f791c7c5e3fa098616ad72f9
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-11 11:44:12 +0530

    Kwargs

--- pythonFundamentals/AdvancePythonConceps.py
+++ pythonFundamentals/AdvancePythonConceps.py
@@ -145,7 +145,6 @@ while(data:=input("Enter the value: ")):
     if data == "q":
         break 
 
-'''
 
 # Args 
 
@@ -156,3 +155,14 @@ def sum(*args):
     return total
 
 print(sum(3,23,5,56,67,5))
+
+'''
+
+#Kwargs
+
+def marks(**kwargs):
+    # kwargs is a dictionary with all the key value pairs which were passed to marks 
+    for item in kwargs.keys():
+        print(f"The marks of {item} is {kwargs[item]}")
+
+marks(shubham=34, vikrant=54, jack=34, Marie=90, Priya=45)
