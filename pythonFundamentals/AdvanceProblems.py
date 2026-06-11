050f8caab65e9d915ff4895d747fb6a8c1e36a72
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-11 16:45:00 +0530

    Question 5

--- pythonFundamentals/AdvanceProblems.py
+++ pythonFundamentals/AdvanceProblems.py
@@ -71,7 +71,6 @@ print(e1.salary)# output 3000
 e1.salary=5000 # output "sets the __salary to 5000"
 print(e1.salary)# output 5000
 
-'''
 
 # Question 4 
 # Create a class MathUtils with:
@@ -93,3 +92,31 @@ class MathUtils:
 
 print(MathUtils.add(2,4))
 print(MathUtils.description())
+
+'''
+
+# Question 5
+
+# Create a class Book with attributes title and author .
+# Implement __str__() so that printing the object displays "Title by
+# Author" .
+# Implement __len__() so that len(book) returns the length of the title.
+# Create two Book objects and test these methods
+
+class Book:
+    def __init__(self, title, author):
+        self.title= title
+        self.author= author
+
+    def __str__(self):
+        return (f'{self.title} is an amazing book by {self.author} ')
+    
+    def __len__(self):
+        return(len(self.title))
+
+b1=Book('paris Adventure','john')
+b2=Book('love in paris','Smith')
+print(str(b1))
+print(len(b1))
+print(str(b2))
+print(len(b2))
