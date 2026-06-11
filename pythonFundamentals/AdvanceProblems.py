9b8e3d4e4f83520b6c7e2c1ff20b0f8dada92ba3
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-11 12:17:51 +0530

    Question 2

--- pythonFundamentals/AdvanceProblems.py
+++ pythonFundamentals/AdvanceProblems.py
@@ -1,4 +1,4 @@
-
+'''
 # Question 1
 # Write a decorator logger that prints "Function is being called" before the function runs. Use it to decorate a function say_hello() that prints "Hello!" .
 
@@ -14,6 +14,29 @@ def say_hello():
 
 say_hello()
 
+'''
 
+# Question 2
 
+# Write a decorator timer that calculates how long a function takes to execute.
+# Test it with a function that sums numbers from 1 to 1,000,000.
+
+from time import time
+
+def timer(func):
+    def wrapper(n):
+        t1 = time()
+        func(n)
+        t2=time()
+        time_taken=f' Time taken to calculate {t2-t1}'
+        return time_taken
+    return wrapper
+
+@timer
+def sum(n):
+    total= 0
+    for i in range(1, n+1):
+        total += i
+    print(f'sum on {n} is {total}')
+print(sum(1000000))
 
