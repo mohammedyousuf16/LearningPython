e71bf27225c73ea2f0c298231f0d335c70e6949e
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-16 14:19:29 +0530

    Question 2

--- File Input output in python/file-IO-problems.py
+++ File Input output in python/file-IO-problems.py
@@ -1,5 +1,4 @@
 '''
-'''
 # Question 1
 # Create a text file notes.txt using Python and write "Learning Python is fun!" into it.
 # Open notes.txt , read its content, and print it to the console.
@@ -10,4 +9,21 @@ with open('notes.txt', 'w') as f:
 
 with open('notes.txt', 'r') as f:
     data= f.read()
+    print(data)
+'''
+
+# Question 2
+# Write a program that writes three lines of text to a file tasks.txt .
+# Open tasks.txt in append mode and add a new line "Task Completed!" .
+# Read the file and print all lines as a list using readlines() .
+
+with open('task.txt', 'w') as f:
+    data= 'This is the first line \nthis is the middle line \nthis is the final and last line '
+    f.write(data)
+
+with open('task.txt', 'a') as f:
+    f.write(f'\n Task Completed!')
+
+with open('task.txt', 'r') as f:
+    data=f.read()
     print(data)
