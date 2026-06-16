525d6e654067da30a22fca53eeae9b91306c22b4
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-16 12:55:25 +0530

    shutil:- move

--- File Input output in python/07_shutil.py
+++ File Input output in python/07_shutil.py
@@ -1,3 +1,8 @@
 import shutil
 # shutil.rmtree('dir') # removes the entire dir with files
-shutil.copy('john.txt', 'jonny.txt')
+# shutil.copy('john.txt', 'jonny.txt') # this copies the content and paste in the destination if file not found creates a new file
+
+# shutil.move('dir/john.txt', 'dir/') # Moves the john.txt inside dir folder
+
+
+
