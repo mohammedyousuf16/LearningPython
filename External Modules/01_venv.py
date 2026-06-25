0401d918d7c2c30abdca5e00c61ecab60c6115f9
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-06-25 11:36:22 +0530

    Virtual Environments and package management

--- External Modules/01_venv.py
+++ External Modules/01_venv.py
@@ -1,3 +1,9 @@
 # v1 - package 1 (pandas)
 # v2 - package 2 (numpy)
 # v3 - package 3 (moviepy)
+
+# To activate virtul environment in mac use
+# source env2/bin/activate
+
+# To activate virtul environment in Windows use
+# .\(env2)\scripts\Activate.ps1
